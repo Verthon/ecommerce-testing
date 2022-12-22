220fc449361431e9cbf4d47a25c995d148516534
Verthon <christopher.sordyl@gmail.com>
2022-12-22 09:27:01 +0100

    feat: add next auth basic config

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,9 +1,13 @@
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 import { CartContent } from "../components/cart-content/cart-content";
 import { Footer } from "../components/footer/Footer";
 import { Navbar } from "../components/navbar/Navbar";
 
 export default function CartPage() {
+	const t = useSession();
+
+	console.log(t);
 	return (
 		<>
 			<Head>
