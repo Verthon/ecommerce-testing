a42def7181c08252c00dcc30a6ca0d4d4dfdc3bf
Verthon <christopher.sordyl@gmail.com>
2022-12-30 22:17:24 +0100

    feat: add new apollo client for auth

--- graphql/apolloClient.ts
+++ graphql/apolloClient.ts
@@ -1,6 +1,11 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  uri: process.env.HYPERGRAPH_CONTENT_API,
-  cache: new InMemoryCache(),
+	uri: process.env.HYPERGRAPH_CONTENT_API,
+	cache: new InMemoryCache(),
+});
+
+export const authorizedApolloClient = new ApolloClient({
+	uri: process.env.HYPERGRAPH_CONTENT_API,
+	cache: new InMemoryCache(),
 });
