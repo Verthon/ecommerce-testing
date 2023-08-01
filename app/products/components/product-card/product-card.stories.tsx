970b3e7c5fd9ee3f8e28f4d03f70e9c19e76bb66
Verthon <christopher.sordyl@gmail.com>
2023-08-01 18:45:50 +0200

    fix: resolve typescript errors

--- app/products/components/product-card/product-card.stories.tsx
+++ app/products/components/product-card/product-card.stories.tsx
@@ -3,33 +3,35 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { ProductCard } from "./product-card";
 
 const meta = {
-	title: "Example/ProductCard",
-	component: ProductCard,
-	parameters: {
-		layout: "centered",
-	},
-	tags: ["autodocs"],
+  title: "Example/ProductCard",
+  component: ProductCard,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
 } satisfies Meta<typeof ProductCard>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-	args: {
-		description: "Description of uber premium glasses",
-		name: "Eyewear Brand Laura",
-		price: "109900",
-		imageUrl:
-			"https://media.graphassets.com/resize=height:300,width:300/ls9CIxIRNWOJofjg7CHL",
-	},
+  args: {
+    description: "Description of uber premium glasses",
+    name: "Eyewear Brand Laura",
+    price: "109900",
+    imageUrl:
+      "https://media.graphassets.com/resize=height:300,width:300/ls9CIxIRNWOJofjg7CHL",
+    slug: "/bob",
+  },
 };
 
 export const LoadingState: Story = {
-	args: {
-		description: "Description of uber premium glasses",
-		name: "Eyewear Brand Laura",
-		price: "109900",
-		imageUrl: "",
-		isLoading: true,
-	},
+  args: {
+    description: "Description of uber premium glasses",
+    name: "Eyewear Brand Laura",
+    price: "109900",
+    imageUrl: "",
+    isLoading: true,
+    slug: "/laura",
+  },
 };
